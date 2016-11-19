d0da58924cbb84e41ef3a2958bc5214607685e57
Jeff Kelley <SlaunchaMan@gmail.com>
2016-11-18 21:52:21 -0500

    Initial Commit

---
+++ Clasp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Clasp
+//
+//  Created by Jeff Kelley on 11/18/16.
+//  Copyright © 2016 Detroit Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
